ba9c2d9a176987e84fb1867c339e26d7188103ec
Lavanya K <lavanyakrishnappa95@gmail.com>
2024-04-11 09:58:10 -0700

    First Project Commit

---
+++ FirstProject/src/com/excelon/app/Caluculator.java
@@ -0,0 +1,14 @@
+package com.excelon.app;
+
+public class Caluculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10;
+		int num2=20;
+		int result=num1+num2;
+		System.out.println("Sum of num1+num2=>"+result);
+
+	}
+
+}
